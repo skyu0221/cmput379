4784b089f27bc0e67b5ebb2f0e87b49ca03fc3f5
skyu0221 <skyu0221@gmail.com>
2017-02-05 02:24:47 -0700

    Update

--- Assign1/findpattern.c
+++ Assign1/findpattern.c
@@ -6,13 +6,10 @@ unsigned int findpattern ( unsigned char   *pattern,
                            unsigned int     patlength,
                            struct patmatch *locations,
                            unsigned int     loclength ) {
-<<<<<<< HEAD
 
 	unsigned int counter = 0;
 
 	return counter;
-=======
-    
 
     unsigned int page;
     //loop through pages and find pattern match (2^32-1)
@@ -23,5 +20,4 @@ int main(){
     
     findpattern(pattern, patlength, locations, loclength);
     return 0;
->>>>>>> 2d5978df7e4bb6a1f766162fad06b29aa5001576
 }
