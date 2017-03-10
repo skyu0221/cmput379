6664cf44e32d6221c8ca5e9760edc8d2ea7bd57b
linda <dzhang4@ualberta.ca>
2017-03-09 18:39:23 -0700

    connection works, need to add functions

--- Assign2/client.c
+++ Assign2/client.c
@@ -9,13 +9,24 @@
 
 #define	 MY_PORT  2222
 
+void send_query(int s, FILE *keyfile);
+
 /* ---------------------------------------------------------------------
  This is a sample client program for the number server. The client and
  the server need not run on the same machine.
  --------------------------------------------------------------------- */
 
-int main()
+int main(int argc, char * argv[])
 {
+	FILE * keyfile;
+	if(argc==4){
+		//hostname, portnumber
+		keyfile=fopen(argv[3], "r+");
+	}
+	else{
+		keyfile=NULL;
+	}
+
 	int	s, number;
 
 	struct	sockaddr_in	server;
@@ -29,29 +40,28 @@ int main()
 		exit (1);
 	}
 
-	while (1) {
+	s = socket (AF_INET, SOCK_STREAM, 0);
 
-		s = socket (AF_INET, SOCK_STREAM, 0);
-
-		if (s < 0) {
-			perror ("Client: cannot open socket");
-			exit (1);
-		}
-
-		bzero (&server, sizeof (server));
-		bcopy (host->h_addr, & (server.sin_addr), host->h_length);
-		server.sin_family = host->h_addrtype;
-		server.sin_port = htons (MY_PORT);
-
-		if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
-			perror ("Client: cannot connect to server");
-			exit (1);
-		}
-
-		read (s, &number, sizeof (number));
-		close (s);
-		fprintf (stderr, "Process %d gets number %d\n", getpid (),
-			ntohl (number));
-		sleep (2);
+	if (s < 0) {
+		perror ("Client: cannot open socket");
+		exit (1);
 	}
+
+	bzero (&server, sizeof (server));
+	bcopy (host->h_addr, & (server.sin_addr), host->h_length);
+	server.sin_family = AF_INET;//host->h_addrtype;
+	server.sin_addr.s_addr=inet_addr(argv[1]);
+	server.sin_port = htons (atoi(argv[2]));
+
+	if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
+		perror ("Client: cannot connect to server");
+		exit (1);
+	}
+
+	read (s, &number, sizeof (number));
+	close (s);
+	fprintf (stderr, "Process %d gets number %d\n", getpid (),
+		ntohl (number));
+	sleep (2);
+	
 }
