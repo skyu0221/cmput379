5a9f4712b0987b5d308b3efae5fb079da131e012
skyu0221 <skyu0221@gmail.com>
2017-03-12 11:17:08 -0600

    p worked

--- Assign2/client.c
+++ Assign2/client.c
@@ -1,127 +1,79 @@
-#include "client.h"
-/*#include <sys/types.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 #define	 MY_PORT  2222
-*/
-//void send_query(int s, FILE *keyfile);
-
 
 /* ---------------------------------------------------------------------
  This is a sample client program for the number server. The client and
  the server need not run on the same machine.
  --------------------------------------------------------------------- */
 
-int main(int argc, char * argv[])
-{
-	FILE * keyfile;
-	if(argc==4){
-		//hostname, portnumber
-		keyfile=fopen(argv[3], "r+");
-	}
-	else{
-		keyfile=NULL;
-	}
+int main() {
 
-	int	s, number;
-	char greeting[10000], user_choice[5];
-	struct	sockaddr_in	server;
-
+	int	                 sock,
+	                     number;
+	char                 message[999999],
+	                     buff_send[999999] = {0};
+	struct	sockaddr_in	 serv_addr;
 	struct	hostent		*host;
 
-	host = gethostbyname ("localhost");
+	host = gethostbyname( "localhost" );
 
-	if (host == NULL) {
-		perror ("Client: cannot get host description");
+	if ( host == NULL ) {
+
+		perror( "Client: cannot get host description" );
 		exit (1);
 	}
 
-	s = socket (AF_INET, SOCK_STREAM, 0);
 
-	if (s < 0) {
-		perror ("Client: cannot open socket");
-		exit (1);
+	sock = socket( AF_INET, SOCK_STREAM, 0 );
+
+	if ( sock < 0 ) {
+
+		perror( "Client: cannot open socket" );
+		exit(1);
 	}
 
-	bzero (&server, sizeof (server));
-	bcopy (host->h_addr, & (server.sin_addr), host->h_length);
-	server.sin_family = AF_INET;//host->h_addrtype;
-	server.sin_addr.s_addr=inet_addr(argv[1]);
-	server.sin_port = htons (atoi(argv[2]));
+	//serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero( &serv_addr, sizeof( serv_addr ) );
+	bcopy( host->h_addr, &( serv_addr.sin_addr ), host->h_length );
 
-	if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
-		perror ("Client: cannot connect to server");
-		exit (1);
+	serv_addr.sin_family = host->h_addrtype;
+	serv_addr.sin_port   = htons(MY_PORT);
+
+	if ( connect( sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr) ) ){
+
+		perror( "Client: cannot connect to server" );
+		exit(1);
 	}
 
-	recv(s, greeting, sizeof(greeting),0);
-	printf("%s",greeting);
+	while ( strlen( message ) == 0  )
+		recv( sock, message, sizeof( message ), 0 );
 
-	while(1){
-		printf("Pick an action: \n");
-		printf("1. Send Query\n");
-		printf("2. Update Entry with Encryption\n");
-		printf("3. Update Entry without Encryption\n");
-		printf("4. Clean Entry\n");
-		printf("5. Exit\n");
+	printf( "%s", message );
 
-		fgets(user_choice, sizeof(user_choice),stdin);
-		if(strncmp(user_choice, "1",1)==0){
-			//send query
-		}
-		if(strncmp(user_choice, "2",1)==0){
-			//update with encryption
-		}
-		if(strncmp(user_choice, "3",1)==0){
-			//update without encryption
-		}
-		if(strncmp(user_choice, "4",1)==0){
-			//clean entry
-		}
-		if(strncmp(user_choice, "5",1)==0){
-			close(s);
-			return 1;
-		}
+	while (1) {
+
+		memset( message, 0, sizeof( message ) );
+
+		printf( "What is the command: " );
+		scanf( "%s", buff_send );
+
+		write( sock, buff_send, strlen( buff_send ) );
+
+		while ( strlen( message ) == 0 )
+			read( sock, message, sizeof( message ) );
+
+		printf( "%s", message );
+
+		//close( sock );
 	}
-	
-}
 
-void send_query(int s, FILE * keyfile){
-	char query[10000];
-	printf("entry of whiteboard: \n");
-	fgets(query,sizeof(query),stdin);
-}
-
-void clean_entry(int s){
-
-}
-
-void plaintextEntry(int s){
-
-}
-
-void encryptedEntry(int s, FILE *keyfile){
-
-}
-
-int encrypt(){
-
-}
-
-int decrypt(){
-
-}
-
-char *base64encode(){
-
-}
-
-char *base64decode(){
-	
+	return 0;
 }
--- Assign2/server.c
+++ Assign2/server.c
@@ -1,64 +1,175 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <netdb.h>
+
 #define	MY_PORT	2222
 
-/* ---------------------------------------------------------------------
- This	is  a sample server which opens a stream socket and then awaits
- requests coming from client processes. In response for a request, the
- server sends an integer number  such	 that  different  processes  get
- distinct numbers. The server and the clients may run on different ma-
- chines.
- --------------------------------------------------------------------- */
+#define BUFFER 999999
 
-int main()
-{
-	int	sock, snew, fromlength, number, outnum;
+int main( int argc, char *argv[] ) {
 
-	struct	sockaddr_in	master, from;
-	char tempmessage[2048], message[2048];
 
-	sock = socket (AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		perror ("Server: cannot open master socket");
-		exit (1);
+	int	               serv_sock,
+	                   clnt_sock,
+	                   opt,
+	                   request_entry;
+    unsigned long long num_entry = 0,
+	                   iterator = 0;
+	bool               read_from_file;
+	struct sockaddr_in serv_addr,
+	                   clnt_addr;
+	ssize_t            reader;
+	socklen_t          clnt_addr_size = sizeof( clnt_addr );
+
+	if ( argc != 4 ) {
+
+		perror( "Server: Number of arguments are wrong" );
+		exit(1);
 	}
 
-	master.sin_family = AF_INET;
-	//127.0.0.1
-	master.sin_addr.s_addr = INADDR_ANY;
-	master.sin_port = htons (MY_PORT);
+	while ( ( opt = getopt( argc, argv, "fn" ) ) != -1 ) {
 
-	if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
-		perror ("Server: cannot bind master socket");
-		exit (1);
+		switch ( opt ) {
+
+			case 'f': read_from_file = true;  break;
+			case 'n': read_from_file = false; break;
+			default:
+				perror( "Server: Cannot find the flag. USAGE(-f,-n)" );
+				exit(1);
+		}
 	}
 
-	number = 0;
+	if ( read_from_file ) {
 
-	listen (sock, 5);
+		FILE               *fp;
+		char               *line = NULL;
+		size_t              len = 0;
 
-	while (1) {
-		fromlength = sizeof (from);
-		snew = accept (sock, (struct sockaddr*) & from, & fromlength);
-		if (snew < 0) {
-			perror ("Server: accept failed");
-			exit (1);
+		fp = fopen( argv[3], "r" );
+
+		if ( fp == NULL ) {
+
+			perror( "Server: cannot find the file" );
+			exit(1);
 		}
 
-        strcpy(tempmessage, "CMPUT379 Whiteboard Server v0\n");
-        //printf(tempmessage);
-        send(snew, tempmessage, sizeof(tempmessage), 0);
+		while ( ( reader = getline( &line, &len, fp ) ) != -1 )
+			num_entry++;
 
-        int n=recv(snew,message,sizeof(message),0);
-        printf("message=%s\n",message);
+		fclose( fp );
 
-        send(snew,message,sizeof(message),0);
-	
-		close (snew);
+	} else
+		num_entry = atoi( argv[3] );
+
+	char *entry[num_entry];
+
+	memset( entry, 0, sizeof( entry ) );
+
+	if ( read_from_file ) {
+
+		FILE               *fp;
+		size_t              len = 0;
+
+		fp = fopen( argv[3], "r" );
+
+		for ( ;iterator < num_entry; iterator++ ) {
+			entry[iterator] = NULL;
+			getline( &entry[iterator], &len, fp );
+			entry[iterator][strlen( entry[iterator] ) - 1] = '\0';
+		}
+
+		fclose( fp );
+
+	} else
+		for ( ;iterator < num_entry; iterator++ )
+			entry[iterator] = NULL;
+
+//	serv_sock = socket ( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+	serv_sock = socket ( AF_INET, SOCK_STREAM, 0 );
+
+	if ( serv_sock < 0 ) {
+
+		perror( "Server: cannot open master socket" );
+		exit(1);
 	}
+
+	serv_addr.sin_family      = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port        = htons( MY_PORT );
+
+	if ( bind( serv_sock, (struct sockaddr*)&serv_addr, sizeof( serv_addr ) ) ){
+
+		perror( "Server: cannot bind master socket" );
+		exit(1);
+	}
+
+	listen( serv_sock, 20 );
+
+	clnt_sock = accept( serv_sock, (struct sockaddr*)&clnt_addr,
+	                    &clnt_addr_size );
+
+	char message[BUFFER] = {0};
+
+	sprintf( message, "CMPUT379 Whiteboard Server v0\n%lld\n", num_entry );
+
+	char request[BUFFER], request_entry_char[BUFFER];
+
+	write( clnt_sock, message, sizeof( message ) );
+
+	while ( true ) {
+
+		memset( request, 0, sizeof( request ) );
+		memset( message, 0, sizeof( message ) );
+		memset( request_entry_char, 0, sizeof( request_entry_char ) );
+		read( clnt_sock, request, sizeof(request) );
+
+		if ( request[0] != '?' && request[0] != '@' ) {
+
+			perror( "Server: command not known" );
+			close( clnt_sock );
+			close( serv_sock );
+			exit(1);
+		}
+
+		iterator = 1;
+
+		while ( request[iterator] != '\0' && request[iterator] != 'p' &&
+		                                     request[iterator] != 'c' ) {
+
+			request_entry_char[iterator - 1] = request[iterator];
+			iterator++;
+		}
+
+		request_entry_char[iterator - 1] = '\0';
+
+		request_entry = atoi( request_entry_char );
+		printf("here\n");
+
+		if ( request_entry > num_entry || request_entry <= 0 )
+			sprintf( message, "!%de14\nNo such entry!\n", request_entry );
+		else if ( request[0] == '?' )
+			sprintf( message, "!%dp%d\n%s\n", request_entry,
+			                             strlen( entry[request_entry - 1] ),
+			                                     entry[request_entry - 1] );
+		else if ( request[0] == '@' ) {
+
+			strcpy( entry[request_entry - 1], &request[iterator + 1] );
+			sprintf( message, "!%de0\n\n", request_entry );
+		}
+
+		write( clnt_sock, message, sizeof( message ) );
+
+	}
+
+	close( clnt_sock );
+	close( serv_sock );
+
+	return 0;
 }
